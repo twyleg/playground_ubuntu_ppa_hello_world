66763b92f6a601c4b4929e8d8d5bb716a395675f
Torsten Wylegala <mail@twyleg.de>
2024-10-22 10:30:01 +0200

    Updated Actions

--- cmake/cmake_install.cmake
+++ cmake/cmake_install.cmake
@@ -1,14 +1,35 @@
 # Copyright (C) 2024 twyleg
 
-install(
-    TARGETS core
-    EXPORT HelloWorldTargets
-    FILE_SET HEADERS
-    LIBRARY DESTINATION lib/hello_world
-    ARCHIVE DESTINATION lib/hello_world
-    INCLUDES DESTINATION include
-    RUNTIME DESTINATION bin
-)
+if(${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.23.0")
+
+    install(
+        TARGETS core
+        EXPORT HelloWorldTargets
+        FILE_SET HEADERS
+        LIBRARY DESTINATION lib/hello_world
+        ARCHIVE DESTINATION lib/hello_world
+        INCLUDES DESTINATION include
+        RUNTIME DESTINATION bin
+    )
+
+else()
+
+    install(
+        TARGETS core
+        EXPORT HelloWorldTargets
+        LIBRARY DESTINATION lib/hello_world
+        ARCHIVE DESTINATION lib/hello_world
+        RUNTIME DESTINATION bin
+    )
+
+    install(
+        FILES
+            "${CMAKE_SOURCE_DIR}/libs/hello_world/core/hello_world.h"
+            "${CMAKE_SOURCE_DIR}/libs/hello_world/core/hello_world_fmt.h"
+        DESTINATION include/hello_world/core
+    )
+
+endif()
 
 install(
     TARGETS hello_world_example
